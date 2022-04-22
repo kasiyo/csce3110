aba6c73b7b11d4a52991540ddeb5eebc4fdf0769
Oriana <orianajb18@outlook.com>
2022-04-22 14:54:39 -0500

    uploading faulty pathfinder

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <stdio.h>
+#include "astar.h"
 using namespace std;
 
 int main(){
-	printf("Hello World\n");
+	int grid[MAX_ROW][MAX_COL] = {
+    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
+    { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+    { 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+    { 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+    { 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 }
+    };
+
+	pair<int, int> source = make_pair(8, 0);
+	pair<int, int> dest = make_pair(0, 0);
+	Astar newAstar;
+	newAstar.generatePath(source, dest, grid);
+
 	return 0;
 }
